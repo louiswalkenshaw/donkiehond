b217f92a6b8e312c40dca64d24a7f6b68fcf2579
Louis <luftwaffeza@gmail. om>
2015-05-15 09:44:56 +0200

    ssss

---
+++ wildfly9/src/main/java/com/louis/wildfly9/jsf/ButtonView.java
@@ -0,0 +1,21 @@
+package com.louis.wildfly9.jsf;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+ 
+@ManagedBean
+public class ButtonView {
+     
+	
+	private String buttonView ="Button?";
+    public void buttonAction(ActionEvent actionEvent) {
+        addMessage("Welcome to Primefaces!!");
+    }
+     
+    public void addMessage(String summary) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary,  null);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
